d2a75d1fb1bd4c2fa4b86e2ca91d8ec815a9cf31
Konate Mamadou <120653410+MamadouKernel@users.noreply.github.com>
2023-03-09 10:53:39 +0000

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {OnInit} from "@angular/core";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ExercicesXPGold';
+
+  constructor(httpCLient: HttpClient) {
+  }
+
+  ngOnInit(){
+
+  }
+}
